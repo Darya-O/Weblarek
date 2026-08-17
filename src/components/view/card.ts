2d752ac9afaa9dcc0241a44ec77ca26b6ef2fc95
Darya Ostreshko <dasha.ostreshko@yandex.ru>
2026-08-17 22:46:24 +0300

    fixed

---
+++ src/components/view/card.ts
@@ -0,0 +1,26 @@
+import { Component } from "../Base/Component";
+import { ensureElement } from "../../utils/utils";
+import { ICardData } from "../../types";
+
+export abstract class Card<T extends ICardData> extends Component<T> {
+  protected titleElement: HTMLElement;
+  protected priceElement: HTMLElement;
+  protected constructor(container: HTMLElement) {
+    super(container);
+    this.titleElement = ensureElement<HTMLElement>(
+      ".card__title",
+      this.container,
+    );
+    this.priceElement = ensureElement<HTMLElement>(
+      ".card__price",
+      this.container,
+    );
+  }
+  set title(value: string) {
+    this.titleElement.textContent = value;
+  }
+  set price(value: number | null) {
+    this.priceElement.textContent =
+      value === null ? "Бесценно" : `${value} синапсов`;
+  }
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,17 @@
-export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+import { categoryMap } from "../utils/constants";
 
-export type TPayment = 'online' | 'cash';
+export type CategoryKey = keyof typeof categoryMap;
 
-export type BuyerValidationErrors = Partial<Record<keyof IBuyer, string>>;
-
+export type ApiPostMethods = "POST" | "PUT" | "DELETE";
 
 export interface IApi {
-    baseUrl: string;
-    get<T extends object>(uri: string): Promise<T>;
-    post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
+  get<T extends object>(uri: string): Promise<T>;
+
+  post<T extends object>(
+    uri: string,
+    data: object,
+    method?: ApiPostMethods,
+  ): Promise<T>;
 }
 
 export interface IProduct {
@@ -20,31 +23,101 @@ export interface IProduct {
   price: number | null;
 }
 
+export type TPayment = "card" | "cash" | "";
+
 export interface IBuyer {
-  payment: TPayment | null;
-  email: string;
-  phone: string;
-  address: string;
-}
-
-// Ответ сервера с товарами
-export interface IProductsResponse {
-  items: IProduct[];
-  total: number;
-}
-
-// Данные для отправки заказа
-export interface IOrderRequest {
   payment: TPayment;
   email: string;
   phone: string;
   address: string;
-  total: number;
-  items: string[];
 }
 
-// Ответ сервера после отправки заказа
-export interface IOrderResponse {
+export interface IProductsResponse {
+  total: number;
+  items: IProduct[];
+}
+
+export interface IOrder extends IBuyer {
+  items: string[];
+  total: number;
+}
+
+export interface IOrderResult {
   id: string;
   total: number;
+}
+
+export type TFormErrors = Partial<Record<keyof IBuyer, string>>;
+
+export interface IHeaderData {
+  counter: number;
+}
+
+export interface IGalleryData {
+  catalog: HTMLElement[];
+}
+
+export interface IModalData {
+  content: HTMLElement;
+}
+
+export interface ICardData {
+  title: string;
+  price: number | null;
+}
+
+export interface ICatalogCardData extends ICardData {
+  category: string;
+  image: string;
+}
+
+export interface ICardActions {
+  onClick?: (event: MouseEvent) => void;
+}
+
+export interface IPreviewCardData extends ICatalogCardData {
+  description: string;
+  buttonText: string;
+  buttonDisabled: boolean;
+}
+
+export interface IBasketCardData extends ICardData {
+  index: number;
+}
+
+export interface IBasketViewData {
+  items: HTMLElement[];
+  total: number;
+}
+
+export interface IFormState {
+  valid: boolean;
+  errors: string;
+}
+
+export interface IFormFieldChange<T> {
+  field: keyof T;
+  value: string;
+}
+
+export interface IOrderFormData {
+  payment: TPayment | null;
+  address: string;
+}
+
+export interface IContactsFormData {
+  email: string;
+  phone: string;
+}
+
+export interface ISuccessData {
+  total: number;
+}
+
+export interface ISuccessActions {
+  onClick?: (event: MouseEvent) => void;
+}
+
+export interface ICardEvent {
+  id: string;
 }
